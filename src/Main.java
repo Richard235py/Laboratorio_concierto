479a113bbf29c9f6c5a12988bb06b164f603b718
Richard Torreblanca <richard.torreblanca@unmsm.edu.pe>
2025-09-24 19:55:34 -0500

    Añadiendo interacción con el usuario

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,45 @@
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Persona persona = new Persona();
-        Tarjeta tarjeta = new Tarjeta(12345);
-        Venta venta = new Venta();
-        Concierto concierto = new Concierto();
-        Zona zona = new Zona("Zona A", 5, 50);
-        Entrada entrada = new Entrada(1);
+        Scanner sc = new Scanner(System.in);
 
+        Persona persona = new Persona();
+        Concierto concierto = new Concierto();
+
+        System.out.print("Ingrese numero de tarjeta: ");
+        int numTarjeta = sc.nextInt();
+        Tarjeta tarjeta = new Tarjeta(numTarjeta);
         persona.registrarTarjeta(tarjeta);
+
+        Venta venta = new Venta();
         persona.comprar(venta);
+
         persona.anularVenta(venta);
+
         persona.eliminarTarjeta(tarjeta);
 
-        concierto.agregarZona("Zona A");
-        concierto.eliminarZona("Zona A");
+        System.out.print("Ingrese nombre de la zona a agregar: ");
+        sc.nextLine();
+        String nombreZona = sc.nextLine();
+        concierto.agregarZona(nombreZona);
 
+        System.out.print("Ingrese nombre de la zona a eliminar: ");
+        String eliminarZona = sc.nextLine();
+        concierto.eliminarZona(eliminarZona);
+
+        Zona zona = new Zona(nombreZona, 5, 50);
         zona.generarEntradas();
         zona.mostrarEntrada();
-        zona.venderEntrada(1);
+
+        System.out.print("Ingrese numero de entrada a vender: ");
+        int numEntrada = sc.nextInt();
+        zona.venderEntrada(numEntrada);
+
+        Entrada entrada = new Entrada(numEntrada);
         entrada.liberar();
+
+        sc.close();
     }
 }
